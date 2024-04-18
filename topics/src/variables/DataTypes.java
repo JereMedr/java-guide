7e3f45975522b2dbae95ee1cf7af5db850588b90
jeremias-medrano <jeremias.medrano@esolutions.com.ar>
2024-04-18 16:56:07 -0300

    update DataTypes, add ObjectExample

--- topics/src/variables/DataTypes.java
+++ topics/src/variables/DataTypes.java
@@ -6,56 +6,56 @@ public class DataTypes {
         System.out.println("Primitives: ");
         byte byteVariable = 100;
         System.out.println("byteVariable: " + byteVariable);
-        System.out.println("Type of byteVariable: " + ((Object) byteVariable).getClass().getName());
+        System.out.println("Type of byteVariable: " + ((ObjectExample) byteVariable).getClass().getName());
         System.out.println("Description of byteVariable: Represents small integers, commonly used in file I/O operations and network communications.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // short example
         short shortVariable = 1000;
         System.out.println("shortVariable: " + shortVariable);
-        System.out.println("Type of shortVariable: " + ((Object) shortVariable).getClass().getName());
+        System.out.println("Type of shortVariable: " + ((ObjectExample) shortVariable).getClass().getName());
         System.out.println("Description of shortVariable: Represents integers in a wider range, often used in mathematical calculations and scientific applications.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // int example
         int intVariable = 100000;
         System.out.println("intVariable: " + intVariable);
-        System.out.println("Type of intVariable: " + ((Object) intVariable).getClass().getName());
+        System.out.println("Type of intVariable: " + ((ObjectExample) intVariable).getClass().getName());
         System.out.println("Description of intVariable: Represents whole numbers within a large range, commonly used for counting, indexing, and arithmetic operations.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // long example
         long longVariable = 1000000000L;
         System.out.println("longVariable: " + longVariable);
-        System.out.println("Type of longVariable: " + ((Object) longVariable).getClass().getName());
+        System.out.println("Type of longVariable: " + ((ObjectExample) longVariable).getClass().getName());
         System.out.println("Description of longVariable: Represents very large integer values, often used for storing timestamps, file sizes, or IDs.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // float example
         float floatVariable = 3.14f;
         System.out.println("floatVariable: " + floatVariable);
-        System.out.println("Type of floatVariable: " + ((Object) floatVariable).getClass().getName());
+        System.out.println("Type of floatVariable: " + ((ObjectExample) floatVariable).getClass().getName());
         System.out.println("Description of floatVariable: Represents decimal numbers with moderate precision, commonly used in graphics, simulations, and scientific calculations.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // double example
         double doubleVariable = 3.14159;
         System.out.println("doubleVariable: " + doubleVariable);
-        System.out.println("Type of doubleVariable: " + ((Object) doubleVariable).getClass().getName());
+        System.out.println("Type of doubleVariable: " + ((ObjectExample) doubleVariable).getClass().getName());
         System.out.println("Description of doubleVariable: Represents decimal numbers with high precision, widely used in engineering, finance, and data analysis.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // char example
         char charVariable = 'A';
         System.out.println("charVariable: " + charVariable);
-        System.out.println("Type of charVariable: " + ((Object) charVariable).getClass().getName());
+        System.out.println("Type of charVariable: " + ((ObjectExample) charVariable).getClass().getName());
         System.out.println("Description of charVariable: Represents a single character, commonly used in text processing, input/output operations, and user interfaces.");
         System.out.println("-----------------------------------------------------------------------" );
 
         // boolean example
         boolean booleanVariable = true;
         System.out.println("booleanVariable: " + booleanVariable);
-        System.out.println("Type of booleanVariable: " + ((Object) booleanVariable).getClass().getName());
+        System.out.println("Type of booleanVariable: " + ((ObjectExample) booleanVariable).getClass().getName());
         System.out.println("Description of booleanVariable: Represents a true or false value, used for logical conditions, decision-making, and control flow in programs.");
         System.out.println("-----------------------------------------------------------------------" );
 
